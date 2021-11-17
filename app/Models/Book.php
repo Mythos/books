01d4eeb631c33192e16223aa4a0273825f68d85d
Mythos <stefanschmid35@googlemail.com>
2021-11-17 06:37:27 +0100

    Add PHPDocs for models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Book
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $isbn
+ * @property string $publication_date
+ * @property int $book_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\BookFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereIsbn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book wherePublicationDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     use HasFactory;
--- app/Models/Series.php
+++ app/Models/Series.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Series
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $status
+ * @property int $total
+ * @property int $category_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\SeriesFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Series newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Series query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereTotal($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Series whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Series extends Model
 {
     use HasFactory;
