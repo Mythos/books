e2c7ccc17ff24c0289f8b935837efad4a8b1f032
Mythos <stefanschmid35@googlemail.com>
2021-11-25 18:04:21 +0100

    Improve layout

--- resources/views/upcoming.blade.php
+++ resources/views/upcoming.blade.php
@@ -1,15 +1,13 @@
-<div>
-    <h1 style="display: inline;">{{ __('Upcoming Releases') }}</h1>
-</div>
-<div class="row" style="padding: 1rem 0;">
+<div class="row" style="padding-bottom: 1.2rem;">
     <div class="col-sm-12 col-md-12 col-lg-12">
         <div class="card shadow-sm mb-12">
+            <div class="card-header">{{ __('Upcoming Releases') }}</div>
             <div class="card-body d-flex flex-column table-responsive" style="height: 250px; overflow-y: scroll;">
                 <table class="table table-hover">
                     <thead>
                         <tr>
                             <th scope="col">{{ __('Publish date') }}</th>
-                            <th scope="col">{{ __('Title') }}</th>
+                            <th scope="col" style="min-width: 25rem;">{{ __('Title') }}</th>
                             <th scope="col">{{ __('ISBN') }}</th>
                         </tr>
                     </thead>
