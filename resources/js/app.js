e521165261a9641e9a3fd4b60eb9b7323c776855
Mythos <stefanschmid35@googlemail.com>
2021-11-29 19:45:25 +0100

    Remove unused code

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,3 @@ require("./bootstrap");
 $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip();
 });
-
-$('a[data-type="book-set-status"]').click(function (e) {
-    e.preventDefault();
-    let $this = $(this);
-    let token = $('meta[name="csrf-token"]').attr("content");
-    $.post($this.attr("href"), {
-        _token: token,
-    }).done(function () {
-        location.reload();
-            return;
-    });
-});
