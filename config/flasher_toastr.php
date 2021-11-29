da455770132cf10c64a7c2d49995527e97203ea4
Mythos <stefanschmid35@googlemail.com>
2021-11-29 20:46:15 +0100

    Remove jquery

---
+++ config/flasher_toastr.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'scripts' => array(
+        'https://cdn.jsdelivr.net/npm/@flasher/flasher-toastr@0.1.3/dist/flasher-toastr.min.js',
+    ),
+    'styles' => array(),
+    'options' => array(
+        'progressBar' => true,
+        'timeOut' => 5000,
+    ),
+);
