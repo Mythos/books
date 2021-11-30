ea9ad7d690ec3cc407f1b0463aa6cdf106e92363
Mythos <stefanschmid35@googlemail.com>
2021-11-30 20:21:29 +0100

    Fix warning

--- resources/views/livewire/series/gallery.blade.php
+++ resources/views/livewire/series/gallery.blade.php
@@ -1,12 +1,15 @@
-<div class="my-2">
-    <h1 style="display: inline;">{{ $category->name }}</h1>
-    <div class="float-end">
-        <a href="{{ route('series.create', [$category]) }}" class="btn btn-link"><i class="fas fa-plus-circle"></i></a>
-        <a href="{{ route('categories.edit', [$category]) }}" class="btn btn-link"><i class="fas fa-edit"></i></a>
+<div>
+    <div class="my-2">
+        <h1 style="display: inline;">{{ $category->name }}</h1>
+        <div class="float-end">
+            <a href="{{ route('series.create', [$category]) }}" class="btn btn-link"><i class="fas fa-plus-circle"></i></a>
+            <a href="{{ route('categories.edit', [$category]) }}" class="btn btn-link"><i class="fas fa-edit"></i></a>
+        </div>
     </div>
-</div>
-<div class="row mt-2">
-    @foreach ($series as $item)
-        @include('series.card', [$item])
-    @endforeach
+    <div class="row mt-2">
+        @foreach ($series as $item)
+            @include('series.card', [$item])
+        @endforeach
+    </div>
+
 </div>
