8ff20fcd2913c62ed35de130d4cfe585d2b1d5d6
Mythos <stefanschmid35@googlemail.com>
2021-11-30 20:19:41 +0100

    Update to new Bootstrap JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 require("./bootstrap");
 
-$(document).ready(() => {
-    $('[data-toggle="tooltip"]').tooltip();
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 });
