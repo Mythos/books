d1f664026de20c3cf9a09ca46a953cf01169f372
Mythos <stefanschmid35@googlemail.com>
2021-12-03 20:01:21 +0100

    Add breadcrumb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active">{{ __('Home') }}</li>
+            </ol>
+        </nav>
         <livewire:series.upcoming-series />
         @foreach ($categories as $category)
             @livewire('series.gallery', [$category])
