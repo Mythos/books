8d5445a297a83b3220bbd9a48b022a92b73b72cc
Mythos <stefanschmid35@googlemail.com>
2022-01-11 18:43:15 +0100

    Finish German translation

--- resources/views/livewire/version.blade.php
+++ resources/views/livewire/version.blade.php
@@ -1,3 +1,4 @@
 <a class="text-muted" href="https://github.com/Mythos/books/releases" target="_blank">
-    {{ __('Version') }} {{ config('app.version') }} @if (!empty($latestVersion)){{ __('(Latest: :version)', ['version' => $latestVersion]) }}@endif
+    {{ __('Version') }} {{ config('app.version') }}
+    @if (!empty($latestVersion)) {{ __('(Latest: :version)', ['version' => $latestVersion]) }}@endif
 </a>
