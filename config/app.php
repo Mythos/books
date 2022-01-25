3db4ea0f6aecb6b12acc176d9fb521d79757a54e
Mythos <stefanschmid35@googlemail.com>
2022-01-25 18:51:02 +0100

    Bump version

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | Current application version.
     |
     */
-    'version' => env('APP_VERSION', '1.2.4'),
+    'version' => env('APP_VERSION', '1.2.5'),
 
     /*
     |--------------------------------------------------------------------------
