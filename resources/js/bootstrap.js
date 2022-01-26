1ffbe3f6b3a579afca777dc6c9661f53805c668f
Mythos <stefanschmid35@googlemail.com>
2022-01-26 13:31:49 +0100

    Add chart.js dependency

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,10 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 window.Swal = require("sweetalert2");
 
 window.Flasher = require("@flasher/flasher");
+
+import Chart from "chart.js/auto";
+window.Chart = Chart;
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
