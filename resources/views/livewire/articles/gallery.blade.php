a24cb11066fb0f95da0c78ec013ad6f6164f1854
Mythos <stefanschmid35@googlemail.com>
2022-01-26 19:53:24 +0100

    Show correct count when filtering

--- resources/views/livewire/articles/gallery.blade.php
+++ resources/views/livewire/articles/gallery.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white shadow-sm rounded my-2 px-3 py-2">
     <div class="my-2">
-        <h1 style="display: inline;">{{ $category->name }} <small class="text-muted">({{ $category->articles->count() }} {{ __('Articles') }})</small></h1>
+        <h1 style="display: inline;">{{ $category->name }} <small class="text-muted">({{ count($articles) }} {{ __('Articles') }})</small></h1>
         <div class="float-end">
             @if ($category->type == 0)
                 <a href="{{ route('articles.create', [$category]) }}" class="btn btn-link"><i class="fas fa-plus-circle"></i></a>
--- resources/views/livewire/series/gallery.blade.php
+++ resources/views/livewire/series/gallery.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white shadow-sm rounded my-2 px-3 py-2">
     <div class="my-2">
-        <h1 style="display: inline;">{{ $category->name }} <small class="text-muted">({{ $category->series->count() }} {{ __('SeriesPlural') }})</small></h1>
+        <h1 style="display: inline;">{{ $category->name }} <small class="text-muted">({{ count($series) }} {{ __('SeriesPlural') }})</small></h1>
         <div class="float-end">
             @if ($category->type == 0)
                 <a href="{{ route('series.create', [$category]) }}" class="btn btn-link"><i class="fas fa-plus-circle"></i></a>
