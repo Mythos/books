ba7980deabf1d4185e0f099b42fe74e002ae87e2
Mythos <stefanschmid35@googlemail.com>
2022-02-22 19:37:51 +0100

    Update laravel

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,12 +5,15 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
-     * @return <string, mixed>
+     * @return array<string, mixed>
      */
     public function definition()
     {
