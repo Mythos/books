3de096ee51e6118b5149850dbf23d977f65de495
Mythos <stefanschmid35@googlemail.com>
2022-05-04 17:50:07 +0200

    Add hyperlinks to gallery titles

--- resources/views/livewire/articles/gallery.blade.php
+++ resources/views/livewire/articles/gallery.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white shadow-sm rounded my-2 px-3 py-2">
     <div class="my-2">
-        <h1 style="display: inline;">{{ $category->name }} <small class="text-muted">({{ count($articles) }} {{ __('Articles') }})</small></h1>
+        <h1 style="display: inline;"><a class="text-dark text-decoration-none" href="{{ route('categories.show', [$category]) }}">{{ $category->name }} <small class="text-muted">({{ count($articles) }} {{ __('Articles') }})</small></a></h1>
         <div class="float-end">
             @if ($category->type == 0)
                 <a href="{{ route('articles.create', [$category]) }}" class="btn btn-link"><span class="fas fa-plus-circle"></span></a>
--- resources/views/livewire/series/gallery.blade.php
+++ resources/views/livewire/series/gallery.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white shadow-sm rounded my-2 px-3 py-2">
     <div class="my-2">
-        <h1 style="display: inline;">{{ $category->name }} <small class="text-muted">({{ count($series) }} {{ __('SeriesPlural') }})</small></h1>
+        <h1 style="display: inline;"><a class="text-dark text-decoration-none" href="{{ route('categories.show', [$category]) }}">{{ $category->name }} <small class="text-muted">({{ count($series) }} {{ __('SeriesPlural') }})</small></a></h1>
         <div class="float-end">
             @if ($category->type == 0)
                 <a href="{{ route('series.create', [$category]) }}" class="btn btn-link"><span class="fas fa-plus-circle"></span></a>
