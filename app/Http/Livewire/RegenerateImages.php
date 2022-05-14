5f47646a7257864023cebcb061da6abd5f1bcb38
Mythos <stefanschmid35@googlemail.com>
2022-05-14 14:26:13 +0200

    Add volumes to regeneration

--- app/Http/Livewire/RegenerateImages.php
+++ app/Http/Livewire/RegenerateImages.php
@@ -15,15 +15,24 @@ class RegenerateImages extends Component
 
     public function regenerate(): void
     {
-        $series = Series::all();
+        $series = Series::with('volumes')->whereNotNull('image_url')->orderBy('id')->get();
         foreach ($series as $item) {
-            $image = ImageHelpers::getImage($item->image_url);
-            if (!empty($image)) {
-                ImageHelpers::storePublicImage($image, $item->image_path . '/cover.jpg', true);
-                $nsfwImage = $image->pixelate(config('images.nsfw.pixelate', 10))->blur(config('images.nsfw.blur', 5))->encode('jpg');
-                ImageHelpers::storePublicImage($nsfwImage, $item->image_path . '/cover_sfw.jpg', true);
+            $this->createAndSaveImage($item);
+            $volumes = $item->volumes->whereNotNull('image_url')->sortBy('id');
+            foreach ($volumes as $volume) {
+                $this->createAndSaveImage($volume);
             }
         }
         toastr()->livewire()->addSuccess(__('Images have been updated'));
+    }
+
+    private function createAndSaveImage($item): void
+    {
+        $image = ImageHelpers::getImage($item->image_url);
+        if (!empty($image)) {
+            ImageHelpers::storePublicImage($image, $item->image_path . '/cover.jpg', true);
+            $nsfwImage = $image->pixelate(config('images.nsfw.pixelate', 10))->blur(config('images.nsfw.blur', 5))->encode('jpg');
+            ImageHelpers::storePublicImage($nsfwImage, $item->image_path . '/cover_sfw.jpg', true);
+        }
     }
 }
