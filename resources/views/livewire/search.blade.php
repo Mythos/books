c35bcee8d0ebef415fee75de2357999b85bdb112
Mythos <stefanschmid35@googlemail.com>
2022-05-15 10:29:25 +0200

    Decrease debounce

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <input type="text" class="form-control" wire:model.debounce.500ms='search' placeholder="{{ __('Search...') }}">
+    <input type="text" class="form-control" wire:model.debounce.250ms='search' placeholder="{{ __('Search...') }}">
 </div>
