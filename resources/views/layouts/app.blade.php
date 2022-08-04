45a5063606c449a436e3b659d089eea6bdeff56e
Mythos <stefanschmid35@googlemail.com>
2022-08-04 06:59:33 +0200

    Remove commented code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                {{-- <li class="nav-item"> --}}
                 <div class="d-block d-md-none my-1" style="width: 100%;">
                     @livewire('search')
                 </div>
-                {{-- </li> --}}
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
