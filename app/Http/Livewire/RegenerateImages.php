243d6a8e6aaae328b38a37a85b25bb91b080ad8e
Mythos <stefanschmid35@googlemail.com>
2022-10-06 02:13:49 +0200

    Add error handling when regenerating images

--- app/Http/Livewire/RegenerateImages.php
+++ app/Http/Livewire/RegenerateImages.php
@@ -6,6 +6,8 @@ use App\Helpers\ImageHelpers;
 use App\Models\Article;
 use App\Models\Publisher;
 use App\Models\Series;
+use Exception;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class RegenerateImages extends Component
@@ -19,19 +21,35 @@ class RegenerateImages extends Component
     {
         $articles = Article::whereNotNull('image_url')->orderBy('id')->get();
         foreach ($articles as $article) {
-            ImageHelpers::createAndSaveArticleImage($article->image_url, $article->image_path);
+            try {
+                ImageHelpers::createAndSaveArticleImage($article->image_url, $article->image_path);
+            } catch (Exception $exception) {
+                Log::error('Error while regenerating image for article ' . $article->name, ['exception' => $exception]);
+            }
         }
         $series = Series::with('volumes')->whereNotNull('image_url')->orderBy('id')->get();
         foreach ($series as $item) {
-            ImageHelpers::createAndSaveCoverImage($item->image_url, $item->image_path);
+            try {
+                ImageHelpers::createAndSaveCoverImage($item->image_url, $item->image_path);
+            } catch (Exception $exception) {
+                Log::error('Error while regenerating image for series ' . $item->name, ['exception' => $exception]);
+            }
             $volumes = $item->volumes->whereNotNull('image_url')->sortBy('id');
             foreach ($volumes as $volume) {
-                ImageHelpers::createAndSaveCoverImage($volume->image_url, $volume->image_path);
+                try {
+                    ImageHelpers::createAndSaveCoverImage($volume->image_url, $volume->image_path);
+                } catch (Exception $exception) {
+                    Log::error('Error while regenerating image for volume ' . $volume->isbn, ['exception' => $exception]);
+                }
             }
         }
         $publishers = Publisher::whereNotNull('image_url')->orderBy('id')->get();
         foreach ($publishers as $item) {
-            ImageHelpers::updatePublisherImage($item, true);
+            try {
+                ImageHelpers::updatePublisherImage($item, true);
+            } catch (Exception $exception) {
+                Log::error('Error while regenerating image for publisher ' . $item->name, ['exception' => $exception]);
+            }
         }
         toastr()->addSuccess(__('Images have been updated'));
     }
