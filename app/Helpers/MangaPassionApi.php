aa7cd4c6a1f4f70874fb48f40f5d8f65fe82724e
Mythos <stefanschmid35@googlemail.com>
2024-03-28 16:41:14 +0100

    Get ISBN by additional /volumes/{id} request

--- app/Helpers/MangaPassionApi.php
+++ app/Helpers/MangaPassionApi.php
@@ -13,79 +13,75 @@ class MangaPassionApi
 
     public static function loadSeriesByTitle(string $title)
     {
-        $response = Http::get(MangaPassionApi::API_URL . '/editions?order[titleLength]=asc&order[title]=asc&title=' . urlencode($title));
-        if (!$response->successful()) {
+        $volumeResult = Http::get(MangaPassionApi::API_URL . '/editions?order[titleLength]=asc&order[title]=asc&title=' . urlencode($title));
+        if (!$volumeResult->successful()) {
             return null;
         }
-        $response = $response->json();
-        if (empty($response)) {
+        $volumeResult = $volumeResult->json();
+        if (empty($volumeResult)) {
             return null;
         }
-        $result = $response[0];
-        $series = [];
-        $series['mangapassion_id'] = $result['id'];
+        $result = $volumeResult[0];
+        $volume = [];
+        $volume['mangapassion_id'] = $result['id'];
 
         return MangaPassionApi::loadSeriesById($result['id']);
     }
 
     public static function loadSeriesById(int $mangaPassionId)
     {
-        $response = self::request($mangaPassionId);
-        if (!$response->successful()) {
-            return null;
-        }
-        $result = $response->json();
+        $result = self::seriesRequest($mangaPassionId);
         if (empty($result)) {
             return null;
         }
-        $series = [];
-        $series['mangapassion_id'] = $result['id'];
+        $volume = [];
+        $volume['mangapassion_id'] = $result['id'];
 
-        $series['name'] = $result['title'];
-        $series['description'] = $result['description'] ?? '';
-        $series['status'] = $result['status'] ?? SeriesStatus::ANNOUNCED;
-        $series['image_url'] = $result['cover'] ?? '';
-        $series['total'] = $result['numVolumes'] ?? 0;
+        $volume['name'] = $result['title'];
+        $volume['description'] = $result['description'] ?? '';
+        $volume['status'] = $result['status'] ?? SeriesStatus::ANNOUNCED;
+        $volume['image_url'] = $result['cover'] ?? '';
+        $volume['total'] = $result['numVolumes'] ?? 0;
 
         if (!empty($result['allInOne'])) {
             $isbn = !empty($result['allInOne']['isbn13']) ? (string) Isbn::of($result['allInOne']['isbn13'])->to13() : null;
             $publish_date = !empty($result['allInOne']['date']) ? new DateTime($result['allInOne']['date']) : null;
             $price = !empty($result['allInOne']['price']) ? floatval($result['allInOne']['price']) / 100.0 : 0.00;
             $pages = !empty($result['allInOne']['pages']) ? $result['allInOne']['pages'] : null;
-            $series['allInOne'] = true;
-            $series['isbn'] = $isbn;
-            $series['publish_date'] = $publish_date?->format('Y-m-d');
-            $series['price'] = $price;
-            $series['pages'] = $pages;
+            $volume['allInOne'] = true;
+            $volume['isbn'] = $isbn;
+            $volume['publish_date'] = $publish_date?->format('Y-m-d');
+            $volume['price'] = $price;
+            $volume['pages'] = $pages;
         }
 
         if (!empty($result['sources'])) {
             $source = $result['sources'][0];
-            if (($source['volumes'] ?? 0) > $series['total']) {
-                $series['total'] = $source['volumes'];
+            if (($source['volumes'] ?? 0) > $volume['total']) {
+                $volume['total'] = $source['volumes'];
             }
-            $series['source_status'] = $source['status'] ?? null;
-            $series['source_name'] = $source['title'] ?? $source['romaji'] ?? null;
-            $series['source_name_romaji'] = $source['romaji'] ?? null;
+            $volume['source_status'] = $source['status'] ?? null;
+            $volume['source_name'] = $source['title'] ?? $source['romaji'] ?? null;
+            $volume['source_name_romaji'] = $source['romaji'] ?? null;
             if (!empty($source['tags'])) {
                 $tags = collect($source['tags']);
-                $series['demographics'] = $tags->where('type', '=', '0')->pluck('name')->first();
-                $series['genres'] = $tags->where('type', '=', '1')->pluck('name');
+                $volume['demographics'] = $tags->where('type', '=', '0')->pluck('name')->first();
+                $volume['genres'] = $tags->where('type', '=', '1')->pluck('name');
             }
             if (!empty($source['tags'])) {
                 $magazines = collect($source['magazines']);
-                $series['magazines'] = $magazines->pluck('name');
+                $volume['magazines'] = $magazines->pluck('name');
             }
         }
 
-        $defaultPrice = MangaPassionApi::getDefaultPrice($series['mangapassion_id']);
-        $series['default_price'] = $defaultPrice;
+        $defaultPrice = MangaPassionApi::getDefaultPrice($volume['mangapassion_id']);
+        $volume['default_price'] = $defaultPrice;
 
         if (!empty($result['publishers'])) {
-            $series['publisher'] = $result['publishers'][0]['name'];
+            $volume['publisher'] = $result['publishers'][0]['name'];
         }
 
-        return $series;
+        return $volume;
     }
 
     public static function loadVolumes($mangaPassionId, $total)
@@ -93,31 +89,32 @@ class MangaPassionApi
         $result = [];
         $pages = intval(ceil($total / 100));
         for ($i = 1; $i <= $pages; $i++) {
-            $response = self::request($mangaPassionId, 'volumes?itemsPerPage=100&page=' . $i . '&order[number]=asc');
-            if ($response->successful()) {
-                $responseBody = $response->json();
-
-                foreach ($responseBody as $responseItem) {
-                    if (empty($responseItem['number'])) {
-                        continue;
-                    }
-                    $number = $responseItem['number'];
-                    $publish_date = null;
-                    if ($responseItem['status'] < 2) {
-                        $publish_date = !empty($responseItem['date']) ? new DateTime($responseItem['date']) : null;
-                    }
-                    $isbn = !empty($responseItem['isbn13']) ? (string) Isbn::of($responseItem['isbn13'])->to13() : null;
-                    $price = !empty($responseItem['price']) ? floatval($responseItem['price']) / 100.0 : 0.00;
-                    $pages = $responseItem['pages'] ?? null;
-                    $result[] = [
-                        'number' => $number,
-                        'isbn' => $isbn,
-                        'publish_date' => $publish_date?->format('Y-m-d'),
-                        'price' => $price,
-                        'image_url' => $responseItem['cover'] ?? null,
-                        'pages' => $pages,
-                    ];
+            $volumeResult = self::seriesRequest($mangaPassionId, 'volumes?itemsPerPage=100&page=' . $i . '&order[number]=asc');
+            if (empty($volumeResult)) {
+                return $result;
+            }
+            foreach ($volumeResult as $volume) {
+                if (empty($volume['number']) || empty($volume['id'])) {
+                    continue;
                 }
+                $volume = self::volumeRequest($volume['id']);
+                $number = $volume['number'];
+                $publish_date = null;
+                if ($volume['status'] < 2) {
+                    $publish_date = !empty($volume['date']) ? new DateTime($volume['date']) : null;
+                }
+                $isbn = !empty($volume['isbn13']) ? (string) Isbn::of($volume['isbn13'])->to13() : null;
+                $price = !empty($volume['price']) ? floatval($volume['price']) / 100.0 : 0.00;
+                $pages = $volume['pages'] ?? null;
+                $cover = $volume['cover'] ?? null;
+                $result[] = [
+                    'number' => $number,
+                    'isbn' => $isbn,
+                    'publish_date' => $publish_date?->format('Y-m-d'),
+                    'price' => $price,
+                    'image_url' => $cover,
+                    'pages' => $pages,
+                ];
             }
         }
 
@@ -126,11 +123,7 @@ class MangaPassionApi
 
     public static function getDefaultPrice($mangaPassionId): ?float
     {
-        $volumesResponse = self::request($mangaPassionId, 'volumes?itemsPerPage=1&order[number]=asc');
-        if (!$volumesResponse->successful()) {
-            return null;
-        }
-        $volumesResult = $volumesResponse->json();
+        $volumesResult = self::seriesRequest($mangaPassionId, 'volumes?itemsPerPage=1&order[number]=asc');
         if (empty($volumesResult)) {
             return null;
         }
@@ -145,13 +138,33 @@ class MangaPassionApi
         return null;
     }
 
-    private static function request($mangaPassionId, $additionalParameters = null)
+    private static function seriesRequest($mangaPassionId, $additionalParameters = null)
     {
         $url = MangaPassionApi::API_URL . '/editions/' . $mangaPassionId;
         if (!empty($additionalParameters)) {
             $url .= '/' . $additionalParameters;
         }
 
-        return Http::get($url);
+        $volumeResult = Http::get($url);
+        if ($volumeResult->successful()) {
+            return $volumeResult->json();
+        }
+
+        return null;
+    }
+
+    private static function volumeRequest($mangaPassionId, $additionalParameters = null)
+    {
+        $url = MangaPassionApi::API_URL . '/volumes/' . $mangaPassionId;
+        if (!empty($additionalParameters)) {
+            $url .= '/' . $additionalParameters;
+        }
+
+        $volumeResult = Http::get($url);
+        if ($volumeResult->successful()) {
+            return $volumeResult->json();
+        }
+
+        return null;
     }
 }
