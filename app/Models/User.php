2770c7e34bcec8f88f738964be47bdc3ee091cd3
Mythos <stefanschmid35@googlemail.com>
2024-04-15 16:36:40 +0200

    Use SHA256 hash instead of MD5

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 /**
@@ -99,7 +100,7 @@ class User extends Authenticatable
         }
 
         return Cache::remember($cacheKey, config('cache.duration'), function () use ($avatarSize) {
-            $hash = md5($this->email);
+            $hash = hash('sha256', trim(Str::lower($this->email)));
             $url = "https://www.gravatar.com/avatar/{$hash}?s={$avatarSize}";
             if (empty($url)) {
                 return null;
